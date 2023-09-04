9729d781e37632fba8132501ff3b092242c71c2a
saharsh-07 <109348366+saharsh-07@users.noreply.github.com>
2023-09-04 17:36:17 +0530

    added functionality of color change on hover

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 function populateGrid(size = 16) {
-const container = document.querySelector(".container");
-container.style.gridTemplateColumns = `repeat(${size}, auto)`;
-container.style.gridTemplateRows = `repeat(${size}, auto)`;
-let amt = size * size
-for (let i = 0; i < amt; i++){
-  const div = document.createElement("div");
-  container.appendChild(div)
-}};
+  const container = document.querySelector(".container");
+  const squares = document.querySelectorAll(".container > div");
+  squares.forEach(div => div.remove());
+  container.style.gridTemplateColumns = `repeat(${size}, auto)`;
+  container.style.gridTemplateRows = `repeat(${size}, auto)`;
+  let amt = size * size
+  for (let i = 0; i < amt; i++){
+    const div = document.createElement("div");
+    container.appendChild(div);
+    div.addEventListener("mouseover", () => div.style.backgroundColor = "red")
+    };
+  
+  };
 
-
-let size = parseInt(window.prompt("input size"));
-
-if ((size > 2) && (size < 100)){
- populateGrid(size)
-}
-else{
-  populateGrid();
+function changeSize() {
+  let size = document.getElementById("size-input").value;
+  if ((size > 2) && (size < 100)){
+    populateGrid(size)
+  }
+  else{
+    populateGrid(16);
+  }
 };
+
+
+function keyEnter(e) {
+  if (e.key === "Enter"){
+  let size = e.target.value;
+  if ((size > 2) && (size < 100)){
+    populateGrid(size)
+  }
+  else{
+    populateGrid(16);
+  }
+}};
